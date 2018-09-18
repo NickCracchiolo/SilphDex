f8d22fa03b38b403d0a6604b7bea17f59ceceb6a
Nick Cracchiolo <nickcracchiolo@nicks-macbook-pro.local>
2018-09-18 15:05:47 -0700

    Updated Code from Apple example on real time Vision with ML Model. (current model accuracy horrible only about 9,000 images in data set)

--- PokemonVGC/Controllers/VisionViewController.swift
+++ PokemonVGC/Controllers/VisionViewController.swift
@@ -11,26 +11,191 @@ import AVFoundation
 import Vision
 import CoreML
 
-class VisionViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
-    var coreDataManager:CoreDataManager!
+
+class VisionViewController: VideoViewController {
+    @IBOutlet weak var classificationLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var coreDataManager:CoreDataManager!
+    private var detectionOverlay: CALayer! = nil
+    
+    // Vision parts
+    private var requests = [VNRequest]()
+    
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            let model = try VNCoreMLModel(for: PokemonModel().model)
+            
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
+    
+    /// - Tag: PerformRequests
+    func updateClassifications(for image: UIImage) {
+        classificationLabel.text = "Classifying..."
         
-        AVCaptureDevice.requestAccess(for: .video) { [weak self] (accepted) in
-            guard let s = self else {
-                return
-            }
-            if accepted {
-                // User accepted permissions
-            } else {
-                // User declined permissions
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                /*
+                 This handler catches general image processing errors. The `classificationRequest`'s
+                 completion handler `processClassifications(_:error:)` catches errors specific
+                 to processing that request.
+                 */
+                print("Failed to perform classification.\n\(error.localizedDescription)")
             }
         }
+    }
+    
+    func processClassifications(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results else {
+                self.classificationLabel.text = "Unable to classify image.\n\(error?.localizedDescription ?? "no description")"
+                return
+            }
+            // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+            let classifications = results as! [VNClassificationObservation]
+            
+            if classifications.isEmpty {
+                self.classificationLabel.text = "Nothing recognized."
+            } else {
+                // Display top classifications ranked by confidence in the UI.
+                let topClassifications = classifications.prefix(2).filter( {$0.confidence >= 0.9} )
+                
+                let descriptions = topClassifications.map { classification in
+                    // Formats the classification for display; e.g. "(0.37) cliff, drop, drop-off".
+                    return String(format: "  (%.2f) %@", classification.confidence, classification.identifier)
+                }
+                print("Classification:\n" + descriptions.joined(separator: "\n"))
+                self.classificationLabel.text = "Classification:\n" + descriptions.joined(separator: "\n")
+            }
+        }
+    }
+    
+    func setupVision() {
+        self.requests = [classificationRequest]
+    }
+    
+    func drawVisionRequestResults(_ results: [Any]) {
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        detectionOverlay.sublayers = nil // remove all the old recognized objects
+        for observation in results where observation is VNClassificationObservation {
+            guard let objectObservation = observation as? VNClassificationObservation else {
+                continue
+            }
+            // Select only the label with the highest confidence.
+            //let topLabelObservation = objectObservation.
+            //print(objectObservation.labels)
+            print("Observation: \(objectObservation.identifier), with confidence: \(objectObservation.confidence)")
+//            let objectBounds = VNImageRectForNormalizedRect(objectObservation., Int(bufferSize.width), Int(bufferSize.height))
+//
+//            let shapeLayer = self.createRoundedRectLayerWithBounds(objectBounds)
+//
+//            let textLayer = self.createTextSubLayerInBounds(objectBounds,
+//                                                            identifier: topLabelObservation.identifier,
+//                                                            confidence: topLabelObservation.confidence)
+//            shapeLayer.addSublayer(textLayer)
+//            detectionOverlay.addSublayer(shapeLayer)
+        }
+        self.updateLayerGeometry()
+        CATransaction.commit()
+    }
+    
+    override func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        let exifOrientation = exifOrientationFromDeviceOrientation()
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: exifOrientation, options: [:])
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
+    
+    override func setupAVCapture() {
+        super.setupAVCapture()
+        
+        // setup Vision parts
+        setupLayers()
+        updateLayerGeometry()
+        setupVision()
+        
+        // start the capture
+        startCaptureSession()
+    }
+    
+    func setupLayers() {
+        detectionOverlay = CALayer() // container layer that has all the renderings of the observations
+        detectionOverlay.name = "DetectionOverlay"
+        detectionOverlay.bounds = CGRect(x: 0.0, y: 0.0, width: bufferSize.width, height: bufferSize.height)
+        detectionOverlay.position = CGPoint(x: rootLayer.bounds.midX, y: rootLayer.bounds.midY)
+        rootLayer.addSublayer(detectionOverlay)
+    }
+    
+    func updateLayerGeometry() {
+        let bounds = rootLayer.bounds
+        var scale: CGFloat
+        
+        let xScale: CGFloat = bounds.size.width / bufferSize.height
+        let yScale: CGFloat = bounds.size.height / bufferSize.width
+        
+        scale = fmax(xScale, yScale)
+        if scale.isInfinite {
+            scale = 1.0
+        }
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        
+        // rotate the layer into screen orientation and scale and mirror
+        detectionOverlay.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: scale, y: -scale))
+        // center the layer
+        detectionOverlay.position = CGPoint (x: bounds.midX, y: bounds.midY)
+        
+        CATransaction.commit()
         
     }
     
-    func detectScene(image: CIImage) {
-        
+    func createTextSubLayerInBounds(_ bounds: CGRect, identifier: String, confidence: VNConfidence) -> CATextLayer {
+        let textLayer = CATextLayer()
+        textLayer.name = "Object Label"
+        let formattedString = NSMutableAttributedString(string: String(format: "\(identifier)\nConfidence:  %.2f", confidence))
+        let largeFont = UIFont(name: "Helvetica", size: 24.0)!
+        formattedString.addAttributes([NSAttributedString.Key.font: largeFont], range: NSRange(location: 0, length: identifier.count))
+        textLayer.string = formattedString
+        textLayer.bounds = CGRect(x: 0, y: 0, width: bounds.size.height - 10, height: bounds.size.width - 10)
+        textLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        textLayer.shadowOpacity = 0.7
+        textLayer.shadowOffset = CGSize(width: 2, height: 2)
+        textLayer.foregroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [0.0, 0.0, 0.0, 1.0])
+        textLayer.contentsScale = 2.0 // retina rendering
+        // rotate the layer into screen orientation and scale and mirror
+        textLayer.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: 1.0, y: -1.0))
+        return textLayer
     }
+    
+    func createRoundedRectLayerWithBounds(_ bounds: CGRect) -> CALayer {
+        let shapeLayer = CALayer()
+        shapeLayer.bounds = bounds
+        shapeLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        shapeLayer.name = "Found Object"
+        shapeLayer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 0.2, 0.4])
+        shapeLayer.cornerRadius = 7
+        return shapeLayer
+    }
+    
 }
---
+++ PokemonVGC/UIKit Extensions/CGImageExtension.swift
@@ -0,0 +1,32 @@
+//
+//  CGImageExtension.swift
+//  PokemonVGC
+//
+//  Created by Nick Cracchiolo on 9/18/18.
+//  Copyright © 2018 Nick Cracchiolo. All rights reserved.
+//
+
+import UIKit
+import ImageIO
+
+extension CGImagePropertyOrientation {
+    /**
+     Converts a `UIImageOrientation` to a corresponding
+     `CGImagePropertyOrientation`. The cases for each
+     orientation are represented by different raw values.
+     - Thanks to Apple Vision + ML Example
+     - Tag: ConvertOrientation
+     */
+    init(_ orientation: UIImage.Orientation) {
+        switch orientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
